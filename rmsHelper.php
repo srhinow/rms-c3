5efb1b999ff88c3c0611fece70960ff875428f80
Alexander Menk <a.menk@imi.de>
2014-10-30 18:19:22 +0100

    Hook gefixt

--- rmsHelper.php
+++ rmsHelper.php
@@ -364,7 +364,8 @@ class rmsHelper extends \Backend
 			{
 				foreach ($GLOBALS['TL_HOOKS']['rmsPublish'] as $callback)
 				{
-					$callback($objData->ref_table, unserialize($objData->data));
+					$this->import($callback[0]);
+					$label = $this->$callback[0]->$callback[1]($objData->ref_table, unserialize($objData->data);
 				}
 			}
 
